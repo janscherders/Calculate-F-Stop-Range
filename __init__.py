06b2028ad57c50bfdf5e796e4c95dbcde8321190
aliasguru <message@aliasguru.eu>
2018-11-11 11:34:55 +0100

    update AddOn description

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
 '''
 Created on 13.01.2017
+Updated to 2.8 on 11.11.2018
 
-@author: renderman
+@author: r.trummer
 '''
 
 bl_info = {
 	"name": "Calculate FStop Range",
 	"author": "Rainer Trummer, based on Troy James Sobotka's work",
-	"version": (0, 2, 0),
-	"blender": (2, 77, 0),
+	"version": (0, 3, 0),
+	"blender": (2, 80, 0),
 	"description": "calculates the dynamic range of an image by comparing darkest and brightest scene referred value",
 	"category": "Image"
 }
@@ -20,6 +21,7 @@ else:
 	import bpy
 	from . import image_calculate_fstop_range as mod
 
+
 def register():
 	#    add a FloatProperty to the image type to store the fstop range
 	bpy.types.Image.fstop_range = bpy.props.FloatProperty()
@@ -27,6 +29,7 @@ def register():
 	bpy.types.Image.pixel_max = bpy.props.FloatProperty()
 
 	bpy.utils.register_module(__name__)
+
 
 def unregister():
 	del bpy.types.Image.fstop_range
